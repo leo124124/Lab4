57f5170bf4ecf6c6fa92d3a12fbca54b9892dd84
Zixin Lei <zixinl2@illinois.edu>
2017-09-27 18:28:00 -0500

    What the fuck is this?

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,26 +1,44 @@
 /*
- * The following code needs a lot of TLC. So give it some!
- *
- * 1. Fix all checkstyle errors
- * 2. Determine what it does (it's going to be tough before you finish #1)
- * 3. Improve the name of the methods and variables
- * 4. Add comments and Javadoc comments where needed
- * 5. Remove unnecessary comments as appropriate
+ * The following code needs a lot of TLC. So give it some! 1. Fix all checkstyle errors 2. Determine
+ * what it does (it's going to be tough before you finish #1) 3. Improve the name of the methods and
+ * variables 4. Add comments and Javadoc comments where needed 5. Remove unnecessary comments as
+ * appropriate
  */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * A class that searches for a string in text.
+ */
+
+public class ReclamationProject {
+
+    /**
+     * @param a asdasd
+     * @param b asdasd
+     * @return whether the word is a strict palindrome or not
+     */
+
+    static String doit(final String a, final String b) {
+
+        String newA = a;
+        String newB = b;
+
+        if (a.length() > b.length()) {
+            newB = a;
+            newA = b;
+        }
+        String r = ""; // I love the ternary operator!
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < newA.length(); i++) {
+            for (int j = newA.length() - i; j > 0; j--) {
+                for (int k = 0; k < newB.length() - j; k++) {
+                    if ((newA.regionMatches(i, b, k, j) && j > r.length())) {
+                        r = newA.substring(i, i + j);
+                    }
+                }
+            } // Ah yeah
+        }
+        return r;
+    }
 }
